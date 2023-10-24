45eb6a7d14362b2c873ba5853ee159f9566bfc9d
haniotak <haniotak@es.net>
2023-10-24 11:35:25 -0700

    small fixes to resource migration

--- backend/src/main/java/net/es/oscars/sb/nso/resv/LegacyPopulator.java
+++ backend/src/main/java/net/es/oscars/sb/nso/resv/LegacyPopulator.java
@@ -1,11 +1,15 @@
 package net.es.oscars.sb.nso.resv;
 
 import lombok.extern.slf4j.Slf4j;
+import net.es.oscars.resv.enums.DeploymentIntent;
+import net.es.oscars.resv.enums.DeploymentState;
 import net.es.oscars.sb.nso.db.NsoQosSapPolicyIdDAO;
 import net.es.oscars.sb.nso.db.NsoSdpIdDAO;
+import net.es.oscars.sb.nso.db.NsoSdpVcIdDAO;
 import net.es.oscars.sb.nso.db.NsoVcIdDAO;
 import net.es.oscars.sb.nso.ent.NsoQosSapPolicyId;
 import net.es.oscars.sb.nso.ent.NsoSdpId;
+import net.es.oscars.sb.nso.ent.NsoSdpVcId;
 import net.es.oscars.sb.nso.ent.NsoVcId;
 import net.es.oscars.resv.db.ConnectionRepository;
 import net.es.oscars.resv.ent.*;
@@ -16,10 +20,7 @@ import net.es.topo.common.dto.nso.enums.NsoVplsSdpPrecedence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Slf4j
 @Component
@@ -35,6 +36,8 @@ public class LegacyPopulator {
 
     @Autowired
     private NsoVcIdDAO nsoVcIdDAO;
+    @Autowired
+    private NsoSdpVcIdDAO nsoSdpVcIdDAO;
 
 
     public void importPssToNso() {
@@ -48,29 +51,44 @@ public class LegacyPopulator {
             // to migrate PSS resources to NSO ones
 
             if (nsoVcIdDAO.findNsoVcIdByConnectionId(c.getConnectionId()).isEmpty()) {
+                // this is a legacy connection that has not been migrated yet
+                c.setDeploymentState(DeploymentState.DEPLOYED);
+                c.setDeploymentIntent(DeploymentIntent.SHOULD_BE_DEPLOYED);
+
                 // we haven't saved an nsoVcId yet.
                 Long scheduleId = c.getReserved().getSchedule().getId();
                 Components cmp = c.getReserved().getCmp();
                 Set<Integer> aluSvcIds = new HashSet<>();
                 List<NsoSdpId> nsoSdpIds = new ArrayList<>();
                 List<NsoQosSapPolicyId> nsoQosSapPolicyIds = new ArrayList<>();
+                List<NsoSdpVcId> nsoSdpVcIds = new ArrayList<>();
 
                 // step 1: collect all
                 for (VlanJunction j : cmp.getJunctions()) {
+                    // first, find the SVC ID then all the SDP ids
+
+                    Map<String, Map<NsoVplsSdpPrecedence, Integer>> sdpByTarget = new HashMap<>();
                     for (CommandParam cp : j.getCommandParams()) {
                         // we MUST have an ALU_SVC_ID, and we MUST have exactly one of them
                         if (cp.getParamType().equals(CommandParamType.ALU_SVC_ID)) {
                             aluSvcIds.add(cp.getResource());
-                        }
 
-                        // we might have VC_IDs; they get saved as nsoSdpIds
-                        if (cp.getParamType().equals(CommandParamType.VC_ID)) {
+
+                        } else if (cp.getParamType().equals(CommandParamType.ALU_SDP_ID)) {
+                            // collect SDP ids and match them to target & precedence
                             NsoVplsSdpPrecedence precedence;;
                             if (cp.getIntent().equals(CommandParamIntent.PRIMARY)) {
                                 precedence  = NsoVplsSdpPrecedence.PRIMARY;
                             } else {
                                 precedence  = NsoVplsSdpPrecedence.SECONDARY;
                             }
+
+
+                            if (!sdpByTarget.containsKey(cp.getTarget())) {
+                                sdpByTarget.put(cp.getTarget(), new HashMap<>());
+                            }
+                            sdpByTarget.get(cp.getTarget()).put(precedence, cp.getResource());
+
                             nsoSdpIds.add(NsoSdpId.builder()
                                     .sdpId(cp.getResource())
                                     .scheduleId(scheduleId)
@@ -79,6 +97,33 @@ public class LegacyPopulator {
                                     .precedence(precedence.toString())
                                     .target(cp.getTarget())
                                     .build());
+                        }
+                    }
+
+                    // loop over these again and match vc-ids to sdp-ids
+                    for (CommandParam cp : j.getCommandParams()) {
+                        if (cp.getParamType().equals(CommandParamType.VC_ID)) {
+                            NsoVplsSdpPrecedence precedence;
+                            if (cp.getIntent().equals(CommandParamIntent.PRIMARY)) {
+                                precedence = NsoVplsSdpPrecedence.PRIMARY;
+                            } else {
+                                precedence = NsoVplsSdpPrecedence.SECONDARY;
+                            }
+                            if (!sdpByTarget.containsKey(cp.getTarget())) {
+                                log.error("unable to locate an SDP id ");
+                            } else if (sdpByTarget.get(cp.getTarget()).containsKey(precedence)) {
+                                log.error("unable to locate an SDP id ");
+                            } else {
+                                Integer sdpId = sdpByTarget.get(cp.getTarget()).get(precedence);
+                                nsoSdpVcIds.add(NsoSdpVcId.builder()
+                                        .vcId(cp.getResource())
+                                        .scheduleId(scheduleId)
+                                        .device(j.getDeviceUrn())
+                                        .connectionId(c.getConnectionId())
+                                        .sdpId(sdpId)
+                                        .build());
+
+                            }
                         }
                     }
                 }
@@ -107,8 +152,10 @@ public class LegacyPopulator {
                                 .vcId(pssAluSvcId)
                                 .build());
                     }
+
                     nsoSdpIdDAO.saveAll(nsoSdpIds);
                     nsoQosSapPolicyIdDAO.saveAll(nsoQosSapPolicyIds);
+                    nsoSdpVcIdDAO.saveAll(nsoSdpVcIds);
                 } else {
                     log.error("multiple ALU svc-ids for "+c.getConnectionId()+"; skipping it.");
                 }
