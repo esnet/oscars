ebb190cb75fa5e4971c64c2ffd639cab46e92ce0
Evangelos Chaniotakis <haniotak@gmail.com>
2023-11-17 15:40:44 -0800

    bump version to 1.1.3 (#441)
    get backend version from buildinfo

--- backend/src/main/java/net/es/oscars/web/rest/MiscController.java
+++ backend/src/main/java/net/es/oscars/web/rest/MiscController.java
@@ -2,30 +2,29 @@ package net.es.oscars.web.rest;
 
 import lombok.extern.slf4j.Slf4j;
 import net.es.oscars.app.exc.StartupException;
-
-
-import net.es.oscars.app.util.AppVersion;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-
-
 
 @RestController
 @Slf4j
 public class MiscController {
+    private final BuildProperties buildProperties;
 
+    public MiscController(BuildProperties buildProperties) {
+        this.buildProperties = buildProperties;
+    }
 
 
     @ExceptionHandler(StartupException.class)
     @ResponseStatus(value = HttpStatus.SERVICE_UNAVAILABLE)
-    public void handleStartup(StartupException ex) {
+    public void handleStartup() {
         log.warn("Still in startup");
     }
 
     @RequestMapping(value = "/api/version", method = RequestMethod.GET)
     public String getVersion() {
-        return AppVersion.getVersion();
+        return "%s (%s)".formatted(buildProperties.getVersion(), buildProperties.getTime().toString());
     }
 
     @RequestMapping(value = "/api/ping", method = RequestMethod.GET)
