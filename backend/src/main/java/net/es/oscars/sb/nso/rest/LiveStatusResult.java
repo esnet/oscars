3cf79a6df4c0b41efce629553a022246d41da547
haniotak <haniotak@gmail.com>
2024-02-16 08:46:26 -0800

    simplify some of the MAC code, try to figure out why we are getting nulls

--- backend/src/main/java/net/es/oscars/sb/nso/rest/LiveStatusResult.java
+++ backend/src/main/java/net/es/oscars/sb/nso/rest/LiveStatusResult.java
@@ -5,10 +5,12 @@ import java.time.Instant;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class LiveStatusResult {
