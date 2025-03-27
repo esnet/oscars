58fe5fd63bef149c1fb4fadabff098d947a0d337
Alejandro Albino <aalbino@lbl.gov>
2025-03-26 23:19:53 -0700

    Hot fix to support HTTP compression (GZIP) in ClientUtil.createRequesterClient() method, which uses JaxWx Client object to call HTTP POST

--- backend/src/main/java/net/es/oscars/soap/ClientUtil.java
+++ backend/src/main/java/net/es/oscars/soap/ClientUtil.java
@@ -6,6 +6,7 @@ import net.es.nsi.lib.soap.gen.nsi_2_0.services.point2point.ObjectFactory;
 import net.es.nsi.lib.soap.gen.nsi_2_0.connection.requester.ConnectionRequesterPort;
 import net.es.oscars.app.exc.NsiException;
 import net.es.oscars.app.props.NsiProperties;
+import net.es.oscars.app.props.ClientUtilProperties;
 import net.es.oscars.nsi.beans.NsiErrors;
 import net.es.oscars.nsi.ent.NsiRequesterNSA;
 import org.apache.cxf.configuration.jsse.TLSClientParameters;
@@ -13,6 +14,7 @@ import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.ext.logging.LoggingFeature;
 import org.apache.cxf.frontend.ClientProxy;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.apache.cxf.transport.common.gzip.GZIPInInterceptor;
 import org.apache.cxf.transport.http.HTTPConduit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -39,6 +41,9 @@ public class ClientUtil {
     @Autowired
     private NsiProperties nsiProps;
 
+    @Autowired
+    private ClientUtilProperties clientUtilProps;
+
     HashMap<String, ConnectionRequesterPort> requesterPorts = new HashMap<String, ConnectionRequesterPort>();
 
 
@@ -49,6 +54,18 @@ public class ClientUtil {
      * @return the ConnectionRequesterPort that you can use at the client
      */
     public ConnectionRequesterPort createRequesterClient(NsiRequesterNSA requesterNSA) throws NsiException {
+        // if withGzipCompression is not set, default to application.properties setting for
+        // client-util.enable-gzip-compression flag.
+        return createRequesterClient(requesterNSA, false);
+    }
+    /**
+     * Creates a client for interacting with an NSA requester
+     *
+     * @param requesterNSA the details of the requester to contact
+*
+     * @return the ConnectionRequesterPort that you can use at the client
+     */
+    public ConnectionRequesterPort createRequesterClient(NsiRequesterNSA requesterNSA, boolean withGzipCompression) throws NsiException {
         if (this.requesterPorts.containsKey(requesterNSA.getCallbackUrl())) {
             return this.requesterPorts.get(requesterNSA.getCallbackUrl());
         }
@@ -74,6 +91,17 @@ public class ClientUtil {
 
         ConnectionRequesterPort port = (ConnectionRequesterPort) fb.create();
         Client client = ClientProxy.getClient(port);
+
+        // Enable GZIP compression with appropriate headers in JaxWs Client if
+        // client-util.enable-gzip-compression=true
+        // or
+        // withGzipCompression argument is true.
+        if (clientUtilProps.enableGzipCompression || withGzipCompression) {
+            log.debug("ClientUtil.createRequesterClient() - Gzip compression enabled");
+            client.getInInterceptors().add(new GZIPInInterceptor());
+            client.getOutInterceptors().add(new GZIPInInterceptor());
+        }
+
         this.configureConduit(client, requesterNSA);
 
         this.requesterPorts.put(requesterNSA.getCallbackUrl(), port);
