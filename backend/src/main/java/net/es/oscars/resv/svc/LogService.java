0082962326089321dea6905fc740c5eb25d82c82
haniotak <haniotak@gmail.com>
2025-05-15 08:32:29 -0700

    add @Transactional to LogService misc code style changes, bump version & changelog

--- backend/src/main/java/net/es/oscars/resv/svc/LogService.java
+++ backend/src/main/java/net/es/oscars/resv/svc/LogService.java
@@ -7,8 +7,8 @@ import net.es.oscars.resv.db.LogRepository;
 import net.es.oscars.resv.ent.Event;
 import net.es.oscars.resv.ent.EventLog;
 import net.es.oscars.resv.enums.EventType;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
 import java.util.ArrayList;
@@ -19,9 +19,13 @@ import java.util.ArrayList;
 @Data
 public class LogService {
 
-    @Autowired
-    private LogRepository logRepo;
+    private final LogRepository logRepo;
 
+    public LogService(LogRepository logRepo) {
+        this.logRepo = logRepo;
+    }
+
+    @Transactional
     public void logEvent(String connectionId, Event ev) {
         EventLog eventLog = logRepo.findByConnectionId(connectionId)
                 .orElseGet(() -> EventLog.builder()
