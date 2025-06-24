f90e08151b52f2a1300ddb68182d8f4f6de148b1
haniotak <haniotak@gmail.com>
2025-06-24 10:18:28 -0700

    update spring ver, fix path variable

--- backend/src/main/java/net/es/oscars/web/rest/v2/EseApiController.java
+++ backend/src/main/java/net/es/oscars/web/rest/v2/EseApiController.java
@@ -54,7 +54,7 @@ public class EseApiController {
     @RequestMapping(value = "/api/l2vpn/get/{connectionId}", method = RequestMethod.GET)
     @ResponseBody
     @Transactional
-    public L2VPN get(@RequestBody String connectionId) throws StartupException, ConnException {
+    public L2VPN get(@PathVariable String connectionId) throws StartupException, ConnException {
         startup.startupCheck();
         return l2VPNService.get(connectionId);
     }
