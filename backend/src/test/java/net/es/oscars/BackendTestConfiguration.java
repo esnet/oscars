dd26841b0610b02b2e0797210aaababd023700a8
Alejandro Albino <aalbino@lbl.gov>
2025-07-02 09:11:35 -0700

    Adding inital test setup for TopoSearchController. Restoring test configuration to disable OAuth2ClientConfiguration during testing

--- backend/src/test/java/net/es/oscars/BackendTestConfiguration.java
+++ backend/src/test/java/net/es/oscars/BackendTestConfiguration.java
@@ -1,6 +1,8 @@
 package net.es.oscars;
 
 import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootContextLoader;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +10,7 @@ import org.springframework.test.context.ContextConfiguration;
 
 @Configuration
 @ComponentScan(lazyInit = true)
+@EnableAutoConfiguration(exclude={OAuth2ClientAutoConfiguration.class})
 @ContextConfiguration(
         loader = SpringBootContextLoader.class,
         classes = Backend.class
---
+++ backend/src/test/resources/net/es/oscars/cuke/topo_search_controller.happy.feature
@@ -0,0 +1,9 @@
+@TopoSearchControllerSteps
+@TopoSearchControllerStepsHappy
+Feature: TopoSearchController Endpoints (Happy)
+
+#  Scenario: Perform an edge-port search in the TopoSearchController
+#    Given The client executes POST with PortSearchRequest payload on TopoSearchController path "/api/topo/edge-port-search"
+#    When The client receives a response from TopoSearchController
+#    Then The client receives a TopoSearchController response status code of 200
+#    And The TopoSearchController response is a valid list of EdgePort objects
